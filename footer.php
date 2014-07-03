e5c4d515b7ff33ccb94edd98165e2845b35ae87a
Tristan Ader <tristanader@gmail.com>
2014-07-03 16:16:33 -0500

    tweaked footer, added contact to options

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 <footer class="main-footer">
-    <p>&copy; PARC Office LLC 2014, 20 Grand Ave #703, Brooklyn NY 11205 <strong><em>T</em></strong> 212 991 8662 <strong><em>E</em></strong> <a class="footer-link" href="mailto:office@p-a-r-c.com">office@p-a-r-c.com</a></p>
+    <p>&copy; 
+    	<?php if (get_field('legal_name', 'options')) the_field('legal_name', 'options'); ?> <?php echo date('Y'); ?>, <?php if (get_field('address_1', 'options')) the_field('address_1', 'options'); ?> <?php if (get_field('address_2', 'options')) the_field('address_2', 'options'); ?>, <?php if (get_field('city', 'options')) the_field('city', 'options'); ?> <?php if (get_field('state', 'options')) the_field('state', 'options'); ?> <?php if (get_field('zip', 'options')) the_field('zip', 'options'); ?> <strong><em>T</em></strong> <?php if (get_field('phone', 'options')) the_field('phone', 'options'); ?> <strong><em>E</em></strong> 
+			<?php if (get_field('email', 'options')) : ?>
+    			<a class="footer-link" href="mailto:<?php the_field('email', 'options'); ?>"><?php the_field('email', 'options'); ?></a>
+    		<?php endif; ?>
+    </p>
     <ul class="social-media">
         <li><a href="http://www.linkedin.com/company/parc-office" class="linkedin ir" target="_blank">Follow on LinkedIn</a></li>
         <li><a href="https://twitter.com/willprince" class="twitter ir" target="_blank">Follow on Twitter</a></li>
