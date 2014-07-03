b58c5109295fdcf21acf9dfb5fadc4b6273c8b46
Tristan Ader <tristanader@gmail.com>
2014-07-03 13:17:19 -0500

    added typekit fonts

--- functions.php
+++ functions.php
@@ -1,6 +1,23 @@
 <?php
 
 /**
+ * Add TypeKit Fonts temp
+ * roll it into one below
+ *
+ */
+function theme_typekit() {
+    wp_enqueue_script( 'theme_typekit', '//use.typekit.net/edp6dfq.js');
+}
+add_action( 'wp_enqueue_scripts', 'theme_typekit' );
+    function theme_typekit_inline() {
+    if ( wp_script_is( 'theme_typekit', 'done' ) ) { ?>
+  	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php }
+}
+add_action( 'wp_head', 'theme_typekit_inline' );
+
+
+/**
  * Enqueue styles and scripts
  */
 
