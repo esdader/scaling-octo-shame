9c4a339418c92db4fe14c17fd4432e555bfd13a4
Tristan Ader <tristanader@gmail.com>
2014-07-04 12:36:52 -0500

    basic stub out of modules

--- index.php
+++ index.php
@@ -1,5 +1,88 @@
 <?php get_header(); ?>
-<div class="l-container">
-	
+<div class="l-container clearfix">
+	<div class="l-panel clearfix">
+		<div class="l-col one">
+			<div class="l-module statement-module">
+				
+			</div>
+			<div class="l-module secondary-image-module">
+				
+			</div>
+		</div>
+		<div class="l-col two">
+			<div class="l-module primary-image-module">
+				
+			</div>
+			<div class="l-module graphic-module">
+				
+			</div>
+			<div class="l-module tertiary-image-module">
+				
+			</div>
+		</div>
+	</div>
+	<div class="l-panel clearfix">
+		<div class="l-col one">
+			<div class="l-module statement-module">
+				
+			</div>
+			<div class="l-module secondary-image-module">
+				
+			</div>
+		</div>
+		<div class="l-col two">
+			<div class="l-module primary-image-module">
+				
+			</div>
+			<div class="l-module graphic-module">
+				
+			</div>
+			<div class="l-module tertiary-image-module">
+				
+			</div>
+		</div>
+	</div>
+	<div class="l-panel clearfix">
+		<div class="l-col one">
+			<div class="l-module statement-module">
+				
+			</div>
+			<div class="l-module secondary-image-module">
+				
+			</div>
+		</div>
+		<div class="l-col two">
+			<div class="l-module primary-image-module">
+				
+			</div>
+			<div class="l-module graphic-module">
+				
+			</div>
+			<div class="l-module tertiary-image-module">
+				
+			</div>
+		</div>
+	</div>
+	<div class="l-panel clearfix">
+		<div class="l-col one">
+			<div class="l-module statement-module">
+				
+			</div>
+			<div class="l-module secondary-image-module">
+				
+			</div>
+		</div>
+		<div class="l-col two">
+			<div class="l-module primary-image-module">
+				
+			</div>
+			<div class="l-module graphic-module">
+				
+			</div>
+			<div class="l-module tertiary-image-module">
+				
+			</div>
+		</div>
+	</div>
 </div>
 <?php get_footer(); ?>
