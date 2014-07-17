cd9a3d9f01c37e69df3fe67de815d518d7281cc7
Tristan Ader <tristanader@gmail.com>
2014-07-17 01:10:24 -0500

    added in live data for office page

--- page-office.php
+++ page-office.php
@@ -1,5 +1,37 @@
 <?php get_header(); ?>
 <div class="l-container clearfix">
-	
+	<article class="l-module contact-module">
+		<h1>Contact</h1>
+    	<p><?php 
+			if (get_field('address_1', 'options')) { 
+				the_field('address_1', 'options');
+				echo ' '; 
+			}
+			if (get_field('address_2', 'options')) { 
+				the_field('address_2', 'options'); 
+				echo '<br>'; 
+			}
+			if (get_field('city', 'options')) { 
+				the_field('city', 'options'); 
+				echo ', '; 
+			}
+			if (get_field('state', 'options')) { 
+				the_field('state', 'options'); 
+				echo ' ';
+			}
+			if (get_field('zip', 'options')) {
+				the_field('zip', 'options');
+			}
+		?></p>
+		<p><?php 
+			if (get_field('phone', 'options')) { 
+				the_field('phone', 'options'); 
+				echo '<br>'; 
+			}
+			if (get_field('email', 'options')) : ?>
+		    	<a class="footer-link" href="mailto:<?php the_field('email', 'options'); ?>"><?php the_field('email', 'options'); ?></a>
+	    	<?php endif; ?>
+	    </p>
+	</article>	
 </div>
 <?php get_footer(); ?>
